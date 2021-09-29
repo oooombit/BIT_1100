6d0924a2103101e2e65e07961b1a7827ce714250
oooombit <rpsnv7.0.6737.0@outlook.com>
2021-09-30 00:16:32 +0800

    Update

--- main.ts
+++ main.ts
@@ -15,9 +15,6 @@ namespace hicbit_control {
     export let X_axis: number = 0;
     export let Y_axis: number = 0;
     export let Z_axis: number = 0;
-    export let X_axis_direction: number = 0;
-    export let Y_axis_direction: number = 0;
-    export let Z_axis_direction: number = 0; 
     
     export enum hicbit_key {
         //% block="up"
@@ -127,12 +124,21 @@ namespace hicbit_control {
                     }
                 }
                 if (cmd_code == 0xC2) {
-                    X_axis = strToNumber(handleCmd.substr(14, 2));      //value1(X轴数值)
-                    Y_axis = strToNumber(handleCmd.substr(18, 2));      //value2(Y轴数值)
-                    Z_axis = strToNumber(handleCmd.substr(22, 2));      //value3(Z轴数值)
-                    X_axis_direction = strToNumber(handleCmd.substr(12, 2));
-                    Y_axis_direction = strToNumber(handleCmd.substr(16, 2));
-                    Z_axis_direction = strToNumber(handleCmd.substr(20, 2));
+                    if (strToNumber(handleCmd.substr(12, 2)) == 1)
+                        X_axis = strToNumber(handleCmd.substr(14, 2)) * -1;      //value1(X轴数值)
+                    else if (strToNumber(handleCmd.substr(12, 2)) == 0)
+                        X_axis = strToNumber(handleCmd.substr(14, 2));
+                    
+                    if (strToNumber(handleCmd.substr(16, 2)) == 1)
+                        Y_axis = strToNumber(handleCmd.substr(18, 2)) * -1;      //value2(Y轴数值)
+                    else if (strToNumber(handleCmd.substr(16, 2)) == 0)
+                        Y_axis = strToNumber(handleCmd.substr(18, 2));
+                    
+                    if (strToNumber(handleCmd.substr(20, 2)) == 1)
+                        Z_axis = strToNumber(handleCmd.substr(22, 2)) * -1;      //value3(Z轴数值)
+                    else if (strToNumber(handleCmd.substr(20, 2)) == 0)
+                        Z_axis = strToNumber(handleCmd.substr(22, 2));
+                    
                 }
                 
             }
